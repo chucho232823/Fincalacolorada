98a863ea2743ada82b8f8dd95e9620300b337ecc
unknown <trafalgar_law23@outlook.com>
2026-01-16 16:31:33 -0600

    precio Sillas

--- public/js/index.js
+++ public/js/index.js
@@ -35,7 +35,7 @@ fetch("/listado-de-eventos")
             nombre: evento.nombre,
             tipo: evento.tipo,
             fecha: evento.fecha,
-            fecha: evento.fechaP,
+            fechaP: evento.fechaP,
             hora: evento.hora,
             subtitulo: evento.subtitulo,
             imagen: evento.imagen,
