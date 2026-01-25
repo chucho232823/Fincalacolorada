e01af76928d25eee77d1d119826e4f11a7cc167d
unknown <trafalgar_law23@outlook.com>
2026-01-25 00:39:46 -0600

    }trova

--- public/js/trovaCliente.js
+++ public/js/trovaCliente.js
@@ -594,7 +594,7 @@ const fondo = document.querySelector('.fondoCompra');
 let listaMesaSilla = [];
 let precios = [];
 let total;
-const sillasBloqueadas = [];
+let sillasBloqueadas = [];
 
 /**
  * Funcion para obtener las mesas consecutivas, obviar las mesas de 2 y 3 sillas
